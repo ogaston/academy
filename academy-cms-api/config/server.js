987354085e568100cc935803912fe76eb9ccbd08
Omar Gaston <omar.gaston.c@gmail.com>
2021-12-13 16:40:58 -0400

    initial changes

---
+++ academy-cms-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1c90db43cbffde85926809c26affb522'),
+    },
+  },
+});
---
+++ academy-cms-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4eb30239-ebb9-47c2-8946-74f5efd4b574'
+};
